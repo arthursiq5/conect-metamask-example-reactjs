e99feac3740f517bff442d12af6343aa9dd75b6f
arthursiq5 <arthur.siq5@gmail.com>
2022-03-27 00:26:49 -0300

    Adicionado componente de visualização do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react";
+import Layout from './components/Layout';
 
 export default function App() {
   return (
     <ChakraProvider>
-      content
+      <Layout>
+        <p style={{color: "white"}}>
+          Hello, World
+        </p>
+      </Layout>
     </ChakraProvider>
   )
 }
