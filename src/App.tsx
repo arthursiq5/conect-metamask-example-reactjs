6c049e6c60ccae36f5a31621a034cccbeca8046d
arthursiq5 <arthur.siq5@gmail.com>
2022-03-27 00:33:19 -0300

    Adicionado botão para conectar a uma carteira

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from './components/Layout';
+import ConnectButton from './components/ConnectButton';
 
 export default function App() {
   return (
     <ChakraProvider>
       <Layout>
-        <p style={{color: "white"}}>
-          Hello, World
-        </p>
+        <ConnectButton />
       </Layout>
     </ChakraProvider>
   )
